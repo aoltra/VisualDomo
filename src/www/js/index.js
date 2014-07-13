ed3e11cfc9dfb46c451947172360ccff43ebae36
Alfredo Oltra <aoltra@gmail.com>
2014-07-13 14:22:52 +0200

    Obtención del BSSID de la wifi

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -13,6 +13,7 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -20,6 +21,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
@@ -28,7 +30,7 @@ var app = {
 
         app.receivedEvent('deviceready');
 
-        // Requesting a file system
+        // Create VisualDomo directories
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
 
             // Successful request of a file system
@@ -40,29 +42,54 @@ var app = {
             helpFile.errorHandler
         );
 
+        app.showMainMenu();
+
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+       
         console.log('Received Event: ' + id);
 
-        var odc1 = new ODControl(1,"odc1","prueba","local.opendomo.com","user","opendomo");
+     //   var odc1 = new ODControl(1,"odc1","prueba","local.opendomo.com","user","opendomo");
 
-        var text = document.createTextNode(odc1.version);
-        console.log('VERSION: ' + text);        
-        var child = document.getElementById('listaodc');
-        child.appendChild(text); 
+     //   var text = document.createTextNode(odc1.version);
+     //   var text = odc1.version;
+     //   console.log('VERSION: ' + text);        
+     //   var child = document.getElementById('listaodc');
+     //   child.appendChild(text); 
 
-
-        
     },
+
+    // Show main menu
+    showMainMenu: function() {
+
+        var networkState = navigator.connection.type;
+
+        if (networkState == Connection.CELL_2G && networkState == Connection.CELL_3G
+            && networkState == Connection.CELL_4G && networkState == Connection.CELL) {
+            $( "#mm-configurethis" ).css( "border", "3px solid red" );
+            $( "#mm-assignlocation" ).css( "border", "3px solid red" );
+        }
+
+        if (networkState == Connection.WIFI) {
+            wifiinfo.getBSSID(
+                function(BSSID) { 
+                    console.log("BSSID: " + BSSID); 
+                },
+                function(error) { 
+                    console.log("Error wifiinfo: " + error); 
+                } );   
+        }
+
+        if (networkState == Connection.NONE || networkState == Connection.UNKNOWN || networkState == Connection.ETHERNET) {
+            $( "#mm-configurethis" ).css( "border", "3px solid red" );
+            $( "#mm-assignlocation" ).css( "border", "3px solid red" );
+            $( "#mm-external" ).css( "border", "3px solid red" );
+        }
+
+    },
+
     // HTTP GET request to access ODControl
     httpGetRequestToODControl: function(url,syn,user,pass,callbackFun) {
         var response;
