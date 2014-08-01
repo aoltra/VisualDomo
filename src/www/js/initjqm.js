8961858d567751123bbde48a13cb4d2d527d7144
Alfredo Oltra <aoltra@gmail.com>
2014-08-01 13:10:27 +0200

    Configuraciones previas JQM
    Se elimina el texto de carga

---
+++ src/www/js/initjqm.js
@@ -0,0 +1,19 @@
+/*
+ * Author:              Alfredo Oltra
+ * email:               aoltra@gmail.com, alfredo@uhurulabs.com
+ *
+ * Start date:          31/7/2014
+ *
+ * Configuraciones previa de JQMobile
+ *
+ * Previous settings JQM
+ */
+
+/* JSLint options */
+/*global $ */
+
+$(document).on("mobileinit", function () {
+    "use strict";
+    
+    $.mobile.loading().hide();
+});
