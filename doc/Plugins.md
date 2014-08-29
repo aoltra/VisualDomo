64d3d6c8a2564c20a11594781ef11dff6481362c
Alfredo Oltra <aoltra@gmail.com>
2014-08-29 15:56:31 +0200

    Carga de imágenes de plantas con plugin camera
    Se simplifica la carga de imágenes de cada una de las plantas
    utilizando el sistema por defecto del dispositivo.

--- doc/Plugins.md
+++ doc/Plugins.md
@@ -10,6 +10,7 @@ Para saber que plugins hay ya instalados.
 |[File](https://github.com/apache/cordova-plugin-file)                              |  1.2.0  | cordova plugin add org.apache.cordova.file                   |
 |[Network-information](https://github.com/apache/cordova-plugin-network-information)| 0.2.10  | cordova plugin add org.apache.cordova.network-information    |
 |[WifiInformation](https://github.com/aoltra/WifiInformation)                       |  0.1.0  | cordova plugin add https://github.com/aoltra/WifiInformation |
+|[Camera](https://git-wip-us.apache.org/repos/asf/cordova-plugin-camera.git)        |  0.3.2  | cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-camera.git |
 
 
 ##Documentación
--- src/www/js/visual.js
+++ src/www/js/visual.js
@@ -11,6 +11,7 @@
 
 /* JSLint options */
 /*global Connection, $, helpFile, wifiinfo, console, LocalFileSystem */
+/*jslint plusplus: true*/
 
 var visual = {
     // Visual Constructor
@@ -36,25 +37,80 @@ var visual = {
         $("#add-floor").click(function () {
             console.log("pulsando +");
             
-            var fileDialogParams = {
-                initialFolder: 'undefined',
-                title: 'Selecciona planta'
-            };
+            $('#popup-conf-floor').popup('open');
+        });
+        
+        $("#popup-conf-floor #button-image").click(function (event) {
             
-            $(":mobile-pagecontainer").pagecontainer("change",
-                                                     'libs/FileDialog/main.html',
-                                                     { transition: "flip",
-                                                       data : fileDialogParams
-                                                     });
+            navigator.camera.getPicture(
+                function (uri) {
+                    var img = $('.thumb-img img'),
+                        imgName = $('#popup-conf-floor #curr-img'),
+                        imgHideName = $('#popup-conf-floor #url');
+
+                    img.css('display: block; visibility: visible');
+                    img.src = uri;
+                    imgName.text(uri);
+                    imgHideName.val(uri);
+
+                    console.log("IMG " + uri);
+                },
+                function (e) {
+                    console.log("Error getting picture: " + e);
+                    document.getElementById('camera_status').innerHTML = "Error getting picture.";
+                },
+                {
+                    quality: 50,
+                    destinationType: navigator.camera.DestinationType.FILE_URI,
+                    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+                }
+            );
         });
             
-      
+        $("#popup-conf-floor #floor-conf-ok").click(function (event) {
+
+            var floor = {};
+
+           
+          
+            $.each($('#popup-conf-floor form').serializeArray(), function () {
+
+                if (floor[this.name]) {
+                    if (!floor[this.name].push) {
+                        floor[this.name] = [floor[this.name]];
+                    }
+                    floor[this.name].push(this.value || '');
+                } else {
+                    floor[this.name] = this.value || '';
+                }
+            });
+            
+            $('#popup-conf-floor').popup('close');
+            
+            $('#popup-conf-floor form')[0].reset();
+            $('#popup-conf-floor #curr-img').text("");
+
+         
+            console.log(JSON.stringify(floor));
+
+            visual.addFloor(floor);
+            
+             
+        });
         
+        // cancel button
+        $("#popup-conf-floor :button").click(function (event) {
+            $('#popup-conf-floor').popup('close');
+            $('#popup-conf-floor form')[0].reset();
+        });
+            
         $("#floor-panel #add-floor").data("entry", { "level": 999 });
     },
     
     addFloor: function (floor) {
         "use strict";
+        
+        console.log("ADD FLOOR");
         
         var insertDiv = "#add-floor",
             floorCanvas;
@@ -80,10 +136,16 @@ var visual = {
             }
         });
         
-        floorCanvas = $("<div class='floor-canvas'><img src='" + floor.URL + "'/><p>" + floor.name + " (" + floor.level + ")</p></div>").insertBefore(insertDiv);
-   
+       // floorCanvas = $("<div class='floor-canvas'><img src='" + floor.image + "'/><p>" + floor.name + " (" + floor.level + ")</p></div>").insertBefore(insertDiv);
+        
+       // visual.getPhoto();
+        
+        floorCanvas = $("<div class='floor-canvas'><img src='" + floor.url + "'/><p>" + floor.name + " (" + floor.level + ")</p></div>").insertBefore(insertDiv);
+        
+        
         $(floorCanvas).data("entry", floor);
          
     }
+        
     
 };
