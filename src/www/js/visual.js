bb8fcba4b7679ad06b9030b311ed2ef7b2b4ca12
Alfredo Oltra <aoltra@gmail.com>
2014-09-02 22:11:27 +0200

    Presentación plano en pantalla

--- src/www/js/visual.js
+++ src/www/js/visual.js
@@ -117,6 +117,18 @@ var visual = {
         });
      
         $("#floor-panel #add-floor").data("entry", { "level": 999 });
+        
+        
+        
+        //BETA
+        var json_config,
+            screen = $.mobile.getScreenHeight(),
+            header = $("#page-visual .ui-header").hasClass("#page-visual ui-header-fixed") ? $("#page-visual .ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight(),
+            footer = $("#page-visual .ui-footer").hasClass("#page-visual ui-footer-fixed") ? $("#page-visual .ui-footer").outerHeight() - 1 : $("#page-visual .ui-footer").outerHeight(),
+            contentCurrent = $("#page-visual .ui-content").outerHeight() - $("#page-visual .ui-content").height(),
+            content = screen - header - footer;// - contentCurrent;
+
+        $("#page-visual .ui-content").height(content);
     },
     
     addFloor: function (floor) {
@@ -163,7 +175,7 @@ var visual = {
         $(floorCanvas).on("taphold",  function (event) {
             console.log("pulsacion larga");
             
-            if (visual.floorEdit != $(this).data("entry").level) {
+            if (visual.floorEdit !== $(this).data("entry").level) {
                 $(".floor-edit-toolbar").slideToggle("fast");
                 $(".floor-edit-toolbar").remove();
                 $(".floor-canvas#L" + visual.floorEdit).removeClass("floor-edit-frame");
@@ -185,16 +197,19 @@ var visual = {
             
             $(".floor-edit-toolbar").slideToggle("fast");
             
-            $(".floor-edit-toolbar #delete-floor").click(function () {
+            $(".floor-edit-toolbar #delete-floor").click(function (e) {
                 console.log("DELETE FLOOR");
                 $('#popup-confirm').popup('open');
                 
                 $("#popup-confirm #delete").click(function () {
                     visual.divEdit.remove();
+                    console.log("DELETE FLOOR2");
                 });
+                
+                e.stopPropagation();
             });
             
-            $(".floor-edit-toolbar #edit-floor").click(function () {
+            $(".floor-edit-toolbar #edit-floor").click(function (e) {
                 console.log("EDIT FLOOR");
                 
                 $('#popup-conf-floor #level').val(visual.divEdit.data("entry").level);
@@ -204,73 +219,137 @@ var visual = {
                 $('#popup-conf-floor #url').val(visual.divEdit.data("entry").url);
                 
                 $('#popup-conf-floor').popup('open');
+                
+                e.stopPropagation();
             });
             
-            $(".floor-edit-toolbar #left-shift-floor").click(function () {
+            $(".floor-edit-toolbar #left-shift-floor").click(function (e) {
                 console.log("LEFT SHIFT FLOOR");
 
                 var floor = visual.divEdit.data("entry"),
                     floortmp,
-                    prevDiv = null;
+                    prevDiv = null,
+                    nd;
                  
                 if (floor.level > 0) {
                     prevDiv = visual.getFloorCanvasDiv(floor.level - 1);
-                    var nd = ".floor-canvas#L" + (floor.level - 1);
+                    nd = ".floor-canvas#L" + (floor.level - 1);
                     
-                     if (prevDiv !== null) {
+                    if (prevDiv !== null) {
                         floortmp = prevDiv.data("entry");
                         floortmp.level++;
-                        prevDiv.data("entry",floortmp);
+                        prevDiv.data("entry", floortmp);
                         prevDiv.find("p").first().text(floortmp.name + " (" + floortmp.level + ")");
-                        $(nd).attr('id','L' + floortmp.level );
-                     }
+                        $(nd).attr('id', 'L' + floortmp.level);
+                    }
+                    
+                    visual.divEdit.remove();
+                    floor.level--;
+                    visual.addFloor(floor);
                 }
-                visual.divEdit.remove();
-                floor.level--;
-                visual.addFloor(floor);
-                 
+                
+                e.stopPropagation();
+               
             });
             
-            $(".floor-edit-toolbar #right-shift-floor").click(function () {
+            $(".floor-edit-toolbar #right-shift-floor").click(function (e) {
                 console.log("RIGHT SHIFT FLOOR");
 
                 var floor = visual.divEdit.data("entry"),
                     floortmp,
-                    nextDiv = null;
+                    nextDiv = null,
+                    nd;
                  
                 if (floor.level < 999) {
                     nextDiv = visual.getFloorCanvasDiv(floor.level + 1);
-                    var nd = ".floor-canvas#L" + (floor.level + 1);
+                    nd = ".floor-canvas#L" + (floor.level + 1);
                     
                     if (nextDiv !== null) {
                         floortmp = nextDiv.data("entry");
                         floortmp.level--;
-                        nextDiv.data("entry",floortmp);
+                        nextDiv.data("entry", floortmp);
                         nextDiv.find("p").first().text(floortmp.name + " (" + floortmp.level + ")");
-                        $(nd).attr('id','L' + floortmp.level );
+                        $(nd).attr('id', 'L' + floortmp.level);
                     }
                     visual.divEdit.remove();
                     floor.level++;
                     visual.addFloor(floor);
                 }
-                 
+                
+                e.stopPropagation();
             });
-            
-            
         });
         
-       
         
-       /* $(floorCanvas).click(function (event) {
-            console.log("pulsacion corta");
+        $(floorCanvas).click(function (event) {
+            console.log("PULSACION CORTA");
              
             $(".floor-edit-toolbar").slideToggle("fast");
             $(".floor-edit-toolbar").remove();
+            $(".floor-canvas#L" + visual.floorEdit).removeClass("floor-edit-frame");
             
             visual.floorEdit = -1;
             visual.divEdit = null;
-    
-        });*/
+            
+            //$('.main-canvas').css("background-image", "url(" + $(this).data("entry").url + ")");
+            //$('#draw-area').resizable();
+            var imageObj = new Image();
+            var ctx = $('.main-canvas')[0].getContext('2d');
+            imageObj.src = $(this).data("entry").url;
+           // ctx.canvas.width = $('.main-canvas').height();
+            var ch = ctx.canvas.height, //$('.main-canvas').height(),
+                cw = ctx.canvas.width, //$('.main-canvas').width(),
+                ri = imageObj.naturalWidth / imageObj.naturalHeight,
+                dx = 0,
+                dy = 0,
+                w,
+                h,
+                maxHeight,
+                maxWidth;
+            
+            $('.main-canvas').attr('width', $('#floor-panel').width());
+            $('.main-canvas').attr('height', $('#page-visual .ui-content').height());
+
+        //  $('.main-canvas')[0].getContext('2d').drawImage(imageObj, (cw - wi) * .5, (ch - hi) * .5, wi, hi);
+
+       /* if (cw > imageObj.naturalWidth)
+        {
+            dx = (cw - imageObj.naturalWidth) * .5;
+            esx = 1;
+        }
+
+        if (ch > imageObj.naturalHeight)
+        {
+            dy = (ch - imageObj.naturalHeight) * .5;
+            esy = 1;
+        } 
+        */
+            h = imageObj.naturalHeight;
+            w = imageObj.naturalWidth;
+            maxWidth = ctx.canvas.width;
+            maxHeight = ctx.canvas.height;
+
+            if (w < h) {
+                h = (h * maxWidth) / w;
+                w = maxWidth;
+            } else {
+                w = (w * maxHeight) / h;
+                h = maxHeight;
+            }
+          /***  
+            ctx.beginPath();
+$('.main-canvas')[0].getContext('2d').moveTo(0,50);
+$('.main-canvas')[0].getContext('2d').lineTo(50,50);
+$('.main-canvas')[0].getContext('2d').stroke();
+        ***/
+            dx = Math.abs(w - maxWidth) * .5;
+            dy = Math.abs(h - maxHeight) * .5;
+
+
+            ctx.drawImage(imageObj, parseInt(dx, 10), parseInt(dy, 10), parseInt(w, 10), parseInt(h, 10));
+
+            /*console.log("IMAGEN FONDO: " +  $(this).data("entry").url + " x: " + imageObj.naturalWidth + " y: " + imageObj.naturalHeight + "  wi: " + wi + " hi: " + hi + " ch: " + ch + " cw:" + cw + "  dx:" + dx + " dy:" + dy + "ctxw: " + ctx.canvas.width + "  ctxh: " + ctx.canvas.height) ; */
+        });
          
     },
     
@@ -286,7 +365,7 @@ var visual = {
             if (data) {
                 console.log("GFCnivel: " + index + "  " + data.level + "  " + level);
                 
-                if (data.level == level) {
+                if (data.level === level) {
                     returnDiv = $(this);
                     
                     console.log("ENCONTRADO  " + data.name);
@@ -297,8 +376,5 @@ var visual = {
         
         return returnDiv;
     }
-    
-    
-    
     
 };
